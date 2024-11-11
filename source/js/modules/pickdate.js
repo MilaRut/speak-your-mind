82a4104dd566ca1ce201bf0e82a3813815548991
Mila Balashova <mila.balashova2312@gmail.com>
2024-11-11 21:45:43 +0300

    add admin login, admin clients and docs list render

--- source/js/modules/pickdate.js
+++ source/js/modules/pickdate.js
@@ -1,35 +1,53 @@
-const choiceDateForm = document.querySelector('#choice-date-form');
+const choiceDateForm = document.querySelectorAll('.choice-date-form');
+
+
+// Функция для форматирования даты
+function renderDate(data) {
+  const schTime = data;
+  const date = new Date(schTime);
+  const language = localStorage.getItem('language') || 'en';
+  let optionsDate;
+
+  if (language === 'en') {
+    optionsDate = {
+      day: 'numeric',
+      month: 'long',
+    };
+  } else {
+    optionsDate = {
+      day: 'numeric',
+      month: 'long',
+    };
+  }
+
+  const formattedDate = date.toLocaleString(language === 'en' ? 'en-US' : 'ru-RU', optionsDate);
+  const finalDate = `${formattedDate}`;
+  return finalDate;
+}
 
 function setChoiceBtn() {
   if (!choiceDateForm) {
     return;
   }
 
-  const slots = choiceDateForm.querySelectorAll('input[type="radio"]');
-  const submitBtn = choiceDateForm.querySelector('button[type="submit"]');
-  const dateTime = submitBtn.querySelector('.doctor__submit-date > time');
-  const errorMsg = choiceDateForm.querySelector('.error-message');
+  choiceDateForm.forEach((form) => {
+    const slots = form.querySelectorAll('input[type="radio"]');
+    const submitBtn = form.querySelector('button[type="submit"]');
+    const dateTime = submitBtn.querySelector('.doctor__submit-date');
+    const errorMsg = form.querySelector('.error-message');
 
-  choiceDateForm.addEventListener('submit', function (e) {
-    const checkedSlot = choiceDateForm.querySelectorAll('input[type="radio"]:checked');
-    if (checkedSlot.length === 0) {
-      e.preventDefault();
-      errorMsg.classList.add('is-active');
-    }
-  });
-
-  slots.forEach((slot) => {
-    slot.addEventListener('change', function () {
-      errorMsg.classList.remove('is-active');
-      submitBtn.disabled = false;
-      submitBtn.classList.add('is-enabled');
-      const currentDatetime = slot.closest('label').querySelector('span').textContent;
-      const currentDay = slot.closest('.doctor__day-wrapper').querySelector('time').textContent;
-      dateTime.textContent = ` ${currentDatetime} ${currentDay} `;
-
+    slots.forEach((slot) => {
+      slot.addEventListener('change', function () {
+        errorMsg.classList.remove('is-active');
+        submitBtn.disabled = false;
+        submitBtn.classList.add('is-enabled');
+        submitBtn.setAttribute('data-id', slot.getAttribute('data-id'));
+        const currentDay = slot.getAttribute('data-time');
+        const currentDatetime = slot.getAttribute('value');
+        dateTime.textContent = ` ${renderDate(currentDay)} ${currentDatetime} `;
+      });
     });
   });
-
 }
 
 export {setChoiceBtn};
