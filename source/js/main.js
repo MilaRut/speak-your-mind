aa269fc401cd321e132c361bdeef8e33e610803a
Mila Balashova <mila.balashova2312@gmail.com>
2024-11-27 23:16:16 +0300

    add photos update admin

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {setWrapperClass} from './modules/wapper';
 import {loadLanguage} from './modules/localisation';
 import {setLangBtn} from './modules/lang-switcher';
-import {setName} from './modules/setName';
+import {setName} from './modules/set-name';
 import {logOut} from './modules/logout';
 import {initSlider} from './modules/slider';
 import {initTabs} from './modules/tabs';
@@ -11,18 +11,18 @@ import {showPassword} from './modules/show-password';
 import {switchReadonly} from './modules/readonly-switch';
 // import {validateTextarea} from './modules/textarea';
 import {showTextInput} from './modules/select-other';
-// import {setActiveItem} from './modules/clients-list';
 import {initActionsBtns} from './modules/clients-btns';
 import {initModal} from './modules/modal';
 import {setClientCard} from './modules/client-card';
 import {initSwiper} from './modules/my-swiper';
 import {setChoiceBtn} from './modules/pickdate';
-// import {showClientProfile} from './modules/admin';
 import {initScheduleDropdown, expandList} from './modules/schedule';
 import {redirectToProfile} from './modules/redirect-profile';
 // TODO: delete
 import {setUtilsBtns} from './modules/utils-btns';
 // END TODO
+import {uploadUserPhoto} from './modules/upload-user-photo';
+import {initAdminAlerts} from './modules/amin-alerts';
 
 const savedLanguage = localStorage.getItem('language') || 'en';
 
@@ -40,6 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
   logOut();
   setLangBtn();
   setName();
+  initAdminAlerts();
 
   // Modules
   // ---------------------------------
@@ -52,18 +53,17 @@ window.addEventListener('DOMContentLoaded', () => {
     switchReadonly();
     // validateTextarea();
     showTextInput();
-    // setActiveItem();
     initActionsBtns();
     initModal();
     setClientCard();
     initSwiper();
     setChoiceBtn();
-    // showClientProfile();
     expandList();
     initScheduleDropdown();
     // TODO: delete
     setUtilsBtns();
     // END TODO
     redirectToProfile();
+    uploadUserPhoto();
   });
 });
